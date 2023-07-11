407ab36e4bea844d02a8ed7e954128eca47ac463
Dmitry <dimakutuzov1234@gmail.com>
2023-07-11 22:48:45 +0400

    Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void MakeCube(int n)
+{
+    int j = 0;       //Временная переменная, нужная для записи куба числа
+    for (int i = 1; i < n; i++)
+    {
+        j = i * i * i;
+        Console.Write(j + ", ");
+    }
+    Console.WriteLine(n * n * n);
+}
+
+int GetImput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetImput("Введите число больше 1: ");
+MakeCube(number);
